574cb7a8fd3203b29da84497bddec0d782887ee8
Andrew Collin <109395254+AndrewCollin@users.noreply.github.com>
2022-08-29 14:40:02 -0700

    - Updated length and width of widgets - Improved GUI

--- HotPlateBurger/AllFrame.Designer.cs
+++ HotPlateBurger/AllFrame.Designer.cs
@@ -44,6 +44,7 @@ namespace HotPlateBurger
             this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.DimGray;
+            this.flowLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.flowLayoutPanel1.Controls.Add(this.widget1);
             this.flowLayoutPanel1.Controls.Add(this.widget2);
             this.flowLayoutPanel1.Controls.Add(this.widget3);
@@ -56,37 +57,37 @@ namespace HotPlateBurger
             // widget1
             // 
             this.widget1.BackColor = System.Drawing.Color.Coral;
-            this.widget1.Location = new System.Drawing.Point(30, 30);
-            this.widget1.Margin = new System.Windows.Forms.Padding(30);
+            this.widget1.Location = new System.Drawing.Point(20, 20);
+            this.widget1.Margin = new System.Windows.Forms.Padding(20);
             this.widget1.Name = "widget1";
-            this.widget1.Size = new System.Drawing.Size(270, 352);
+            this.widget1.Size = new System.Drawing.Size(300, 352);
             this.widget1.TabIndex = 0;
             // 
             // widget2
             // 
             this.widget2.BackColor = System.Drawing.Color.Coral;
-            this.widget2.Location = new System.Drawing.Point(360, 30);
-            this.widget2.Margin = new System.Windows.Forms.Padding(30);
+            this.widget2.Location = new System.Drawing.Point(360, 20);
+            this.widget2.Margin = new System.Windows.Forms.Padding(20);
             this.widget2.Name = "widget2";
-            this.widget2.Size = new System.Drawing.Size(270, 352);
+            this.widget2.Size = new System.Drawing.Size(300, 352);
             this.widget2.TabIndex = 1;
             // 
             // widget3
             // 
             this.widget3.BackColor = System.Drawing.Color.Coral;
-            this.widget3.Location = new System.Drawing.Point(30, 442);
-            this.widget3.Margin = new System.Windows.Forms.Padding(30);
+            this.widget3.Location = new System.Drawing.Point(20, 412);
+            this.widget3.Margin = new System.Windows.Forms.Padding(20);
             this.widget3.Name = "widget3";
-            this.widget3.Size = new System.Drawing.Size(270, 352);
+            this.widget3.Size = new System.Drawing.Size(300, 352);
             this.widget3.TabIndex = 2;
             // 
             // widget4
             // 
             this.widget4.BackColor = System.Drawing.Color.Coral;
-            this.widget4.Location = new System.Drawing.Point(360, 442);
-            this.widget4.Margin = new System.Windows.Forms.Padding(30);
+            this.widget4.Location = new System.Drawing.Point(360, 412);
+            this.widget4.Margin = new System.Windows.Forms.Padding(20);
             this.widget4.Name = "widget4";
-            this.widget4.Size = new System.Drawing.Size(270, 352);
+            this.widget4.Size = new System.Drawing.Size(300, 352);
             this.widget4.TabIndex = 3;
             // 
             // AllFrame
@@ -103,11 +104,10 @@ namespace HotPlateBurger
             this.ResumeLayout(false);
         }
 
-        private HotPlateBurger.Widget widget3;
-        private HotPlateBurger.Widget widget4;
-
         private HotPlateBurger.Widget widget1;
         private HotPlateBurger.Widget widget2;
+        private HotPlateBurger.Widget widget3;
+        private HotPlateBurger.Widget widget4;
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
 
